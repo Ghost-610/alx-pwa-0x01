16b7930f94063e09a7701344de690ea942b8a947
elorm gershon <alidugershon@gmail.com>
2025-08-23 02:32:44 +0000

    Test and Deploy Your PWA (Bonus)

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,29 +1,29 @@
-// pages/_document.tsx
-import { Html, Head, Main, NextScript } from "next/document";
+// // pages/_document.tsx
+// import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en">
-      <Head>
-        {/* PWA Manifest */}
-        <link rel="manifest" href="/manifest.json" />
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         {/* PWA Manifest */}
+//         <link rel="manifest" href="/manifest.json" />
 
-        {/* Theme Color */}
-        <meta name="theme-color" content="#0070f3" />
+//         {/* Theme Color */}
+//         <meta name="theme-color" content="#0070f3" />
 
-        {/* Add icons if needed */}
-        <link rel="icon" href="/icons/android-chrome-192x192.png" />
-        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
-        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+//         {/* Add icons if needed */}
+//         <link rel="icon" href="/icons/android-chrome-192x192.png" />
+//         <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+//         <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
 
-      </Head>
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
-}
+//       </Head>
+//       <body className="antialiased">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
 
 //the original one was commented out
 
@@ -43,3 +43,22 @@ export default function Document() {
 //     </Html>
 //   );
 // }
+
+
+
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html lang="en">
+      <Head>
+        <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#0070f3" />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
