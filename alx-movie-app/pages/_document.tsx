3f7ae5b3164b68c2853e52f5e4fd9dc4e380f297
elorm gershon <alidugershon@gmail.com>
2025-08-23 00:29:59 +0000

    ..............

--- alx-movie-app/pages/_document.tsx
+++ alx-movie-app/pages/_document.tsx
@@ -1,9 +1,22 @@
+// pages/_document.tsx
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        {/* PWA Manifest */}
+        <link rel="manifest" href="/manifest.json" />
+
+        {/* Theme Color */}
+        <meta name="theme-color" content="#0070f3" />
+
+        {/* Add icons if needed */}
+        <link rel="icon" href="/icons/android-chrome-192x192.png" />
+        <link rel="apple-touch-icon" href="/icons/apple-icon-152x152.png" />
+        <meta name="msapplication-TileImage" content="/icons/ms-icon-310x310.png" />
+
+      </Head>
       <body className="antialiased">
         <Main />
         <NextScript />
@@ -11,3 +24,22 @@ export default function Document() {
     </Html>
   );
 }
+
+//the original one was commented out
+
+// import { Html, Head, Main, NextScript } from "next/document";
+
+// export default function Document() {
+//   return (
+//     <Html lang="en">
+//       <Head>
+//         <link rel="manifest" href="/manifest.json" />
+//         <meta name="theme-color" content="#0070f3" />
+//       </Head>
+//       <body className="antialiased">
+//         <Main />
+//         <NextScript />
+//       </body>
+//     </Html>
+//   );
+// }
